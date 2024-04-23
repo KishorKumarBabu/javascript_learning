6394d786ea094f0915ce5ba8727050742c978892
KISHOR KUMAR <161269047+KishorKumarBabu@users.noreply.github.com>
2024-04-23 15:55:06 +0530

    Add files via upload

---
+++ kish.sql
@@ -0,0 +1,6 @@
+show databases;
+create database kishor;
+use kishor;
+create table student(name varchar(20), college varchar(20));
+insert into student value('kishor','ace'),('priya','ace');
+select * from student;
