5753fdc7fdeee22e20466a6e70c475e5598d34c8
kishor kumar <kishorkumarbabu5@gmail.com>
2024-08-03 21:34:08 +0530

    delete

--- kish.sql
+++
@@ -1,7 +0,0 @@
-show databases;
-create database kishor;
-use kishor;
-create table student(name varchar(20), college varchar(20));
-insert into student value('kishor','ace'),('priya','ace');
-select * from student;
-
