d1a3451350cece2d0b0c8ad4a6ac63655e0a1280
kishor kumar <kishorkumarbabu5@gmail.com>
2024-08-08 21:34:01 +0530

    dom manipulation

---
+++ dom manipulation/ex1/script.js
@@ -0,0 +1,5 @@
+//select the paragraph tag
+var para=document.getElementById("title")// it takes only text contant
+console.log(para.textContent)
+para.textContent='bye world'
+console.log(para.textContent)
---
+++ forloop/forloop.js
@@ -0,0 +1,4 @@
+var name="kishor"
+for(i=1;i<=10;i++){
+    console.log(name)
+}
