dc0e21b5a7fd2d21b7cbea1528c21f77cb9aed5f
kishor kumar <kishorkumarbabu5@gmail.com>
2024-08-14 20:09:23 +0530

    stdform

---
+++ studentform/script.js
@@ -0,0 +1,6 @@
+var nam=document.getElementById("nam")
+var ag=document.getElementById("ag")
+var gen=document.getElementById("gen")
+var cour=document.getElementById("cour")
+var data=document.getElementById("data")
+
