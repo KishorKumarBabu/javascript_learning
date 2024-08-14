2a3ca363a974179831cbd25205fdd2a77b89d521
kishor kumar <kishorkumarbabu5@gmail.com>
2024-08-14 21:08:32 +0530

    studentform

--- studentform/script.js
+++ studentform/script.js
@@ -1,6 +1,5 @@
 var nam=document.getElementById("nam")
 var ag=document.getElementById("ag")
-var gen=document.getElementById("gen")
 var cour=document.getElementById("cour")
 var data=document.getElementById("data")
 function stform()
