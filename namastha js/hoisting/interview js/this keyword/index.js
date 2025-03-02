d9e9d9fb0b8f586261546443f5bf829475b4aec5
kishor kumar <kishorkumarbabu5@gmail.com>
2025-03-02 09:49:38 +0530

    js

--- namastha js/hoisting/interview js/this keyword/index.js
+++ namastha js/hoisting/interview js/this keyword/index.js
@@ -1,12 +1,66 @@
-"use strict"
+"use strict";
 //this in global space
-console.log(this)
+console.log(this);
 //this inside a function
-function x(){
-  console.log(this)
+function x() {
+  console.log(this);
 }
-x()
-//this inside non strictmode 
-// if this keyword valu is undefine or null 
+
+//this inside non strictmode
+// if this keyword valu is undefine or null
 //this key word will replace the globle object
 //only in non strict mode
+
+// this keyword value depend on how this is called (window)
+
+x();
+window.x()
+
+//this inside a object's methods
+
+const obj ={
+  a:10,
+  x:function (){
+    console.log(this.a)
+  }
+}
+/* obj.x() */
+
+// call apply bind method (sharing methods)
+
+const student={
+  name:"kishor",
+  printname:function(hometown,state){
+    console.log(this.name+"hometown"+hometown+","+state)
+  }
+}
+student.printname("tirupathur")
+const student2={
+  name:"kumar"
+}
+student.printname.call(student2,"tiruprthur") // here the value of this is student2
+student.printname.apply(student2,["mubai","mharastra"])
+// bind method
+let printmyname = student.printname.bind(student2,["mubai","mharastra"])
+console.log(printmyname)
+printmyname()
+
+// this keyword inside the arrow function
+
+const obje={
+  a:10,
+  x:()=> {
+    console.log(this)
+}
+}
+obje.x()
+// this keyword nested inside the function 
+const obje2={
+  a:10,
+  x:function(){
+   const y=  ()=> {
+    console.log(this)
+}
+y()}
+}
+obje2.x()
